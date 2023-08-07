eab9c0a6c151f41968afe9825770a75afd1f1cb4
mopore <jni@mopore.org>
2023-08-07 08:50:00 +0200

    Reduce footer size.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Link, Typography } from '@mui/material';
 
 function Footer() {
 	return (
-			<Box sx={{ bgcolor: '#1976d2', p: 2, position: 'fixed', bottom: 0, width: '100%', color: '#fff' }}>
+			<Box sx={{ bgcolor: '#1976d2', p: 1, position: 'fixed', bottom: 0, width: '100%', color: '#fff' }}>
 			<Typography variant="body1" align="center">
 			Further details in the <Link href="https://github.com/PRODYNA/marcel_knowhow_main" color="inherit" target="_blank" rel="noopener">GitHub Repository</Link>
 			</Typography>
